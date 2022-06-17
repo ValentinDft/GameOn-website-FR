a533781469b5fd829768256c676003a07d659e35
Valentin Duffet <vavadu07@gmail.com>
2022-06-17 13:56:02 +0200

    Close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,20 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModal = document.getElementById("close_modal");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeModal.addEventListener("click", lauchClosingModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// Close modal form
+function lauchClosingModal() {
+  modalbg.style.display = "none";
 }
 
 
